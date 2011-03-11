6f94c38419d8c3e6132c1c8a33ad3cad27319716
Jesse Alama <jesse.alama@gmail.com>
2011-03-11 13:31:07 +0000

    Generate an HTML listing of today's commits from my repos.

---
+++ todays-commits.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -
+
+repos="hunchentoot-utils sandbox mizar/mizar-items mizar/xsl4mizar mizar/mizarmode mizar/mwiki";
+
+for repo in $repos; do
+    name=`basename $repo`;
+    path=/Users/alama/sources/$repo;
+    commits=`git --git-dir=$path/.git log --since yesterday | grep ^commit | cut -f 2 -d ' '`;
+    if [[ $? -eq "0" ]]; then
+	for commit in $commits; do
+	    msg=`git --git-dir=$path/.git diff-tree -s --pretty=%s $commit`;
+	    if [[ $msg = '' ]]; then
+		echo "<li><a href=\"https://github.com/jessealama/$name/$commit\"><em>(no message was supplied)</em></a>";
+	    else
+		echo "<li><a href=\"https://github.com/jessealama/$name/commits/$commit\">$msg</a></li>";
+	    fi;
+	done
+    else
+	echo "Uh oh: something went wrong calling git on the repo '$repo', with name '$name', at '$path'";
+    fi
+done
+
+exit 0;
