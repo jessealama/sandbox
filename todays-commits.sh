916f0e0ab2074dea5b4acbd688c5a372874cbd2d
Jesse Alama <jesse.alama@gmail.com>
2011-03-13 13:23:08 +0000

    POSIXify.
    Thanks, bashcritic (https://trac.id.ethz.ch/projects/bashcritic/).

--- todays-commits.sh
+++ todays-commits.sh
@@ -7,11 +7,11 @@ for repo in $repos; do
     name=`basename $repo`;
     path=/Users/alama/sources/$repo;
     commits=`git --git-dir=$path/.git log --branches='*' --since yesterday | grep ^commit | cut -f 2 -d ' '`;
-    if [[ $? -eq "0" ]]; then
+    if [ $? -eq "0" ]; then
 	for commit in $commits; do
 	    msg=`git --git-dir=$path/.git diff-tree -s --pretty=%s $commit`;
-	    if [[ $? -eq "0" ]]; then
-		if [[ $msg = '' ]]; then
+	    if [ $? -eq "0" ]; then
+		if [[ $msg = "" ]]; then
 		    echo "<li><a href=\"$github/$name/$commit\"><em>(no message was supplied)</em></a>";
 		else
 		    echo "<li><a href=\"$github/$name/commits/$commit\">$msg</a></li>";
