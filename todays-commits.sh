f87eeda561148ba0d132e95e2ce15e0bdaee050b
Jesse Alama <jesse.alama@gmail.com>
2011-04-02 10:17:03 +0100

    Try to fetch upstream updates before building our list.

--- todays-commits.sh
+++ todays-commits.sh
@@ -3,6 +3,30 @@
 repos="hunchentoot-utils sandbox mizar/mizar-items mizar/xsl4mizar mizar/mizarmode mizar/mwiki dialogues polyhedra dotemacs";
 github="https://github.com/jessealama";
 
+# first, fetch (but don't pull) any new commits 
+
+cannot_fetch=""; # repos from which we cannot fetch the latest commits
+
+echo -n "Fetching new commits...";
+
+for repo in $repos; do
+    name=`basename $repo`;
+    path=/Users/alama/sources/$repo;
+    git --git-dir=$path/.git fetch --quiet origin > /dev/null 2>&1
+    fetch_exit_code=$?;
+    if [ ! $fetch_exit_code -eq "0" ]; then
+	cannot_fetch="$repo $cannot_fetch";
+    fi
+done
+
+echo "done.";
+
+if [ ! "$cannot_fetch" = "" ]; then # something went wrong
+    echo "Unable to fetch any new commits from github for the repos:";
+    echo "  $cannot_fetch";
+    echo "The following information may not accurately reflect your latest work."
+fi
+
 for repo in $repos; do
     name=`basename $repo`;
     path=/Users/alama/sources/$repo;
