2142c2aa7fe0f04a1761a23a9e23859f6d8eae0b
Jesse Alama <jesse.alama@gmail.com>
2011-04-02 10:37:03 +0100

    Use definition lists.

--- todays-commits.sh
+++ todays-commits.sh
@@ -50,20 +50,23 @@ if [ ! "$uncommitted" = "" ]; then
     exit 1;
 fi
 
-echo "<ul>";
+echo "<dl>";
 for repo in $repos; do
     name=`basename $repo`;
     path=/Users/alama/sources/$repo;
     commits=`git --git-dir=$path/.git log --branches='*' --since yesterday | grep ^commit | cut -f 2 -d ' '`;
     if [ $? -eq "0" ]; then
+	echo "<dt>$name</dt>";
 	for commit in $commits; do
 	    msg=`git --git-dir=$path/.git diff-tree -s --pretty=%s $commit`;
 	    if [ $? -eq "0" ]; then
+		echo -n "<dd>";
 		if [[ $msg = "" ]]; then
-		    echo "<li><a href=\"$github/$name/$commit\"><em>(no message was supplied)</em></a>";
+		    echo -n "<a href=\"$github/$name/$commit\"><em>(no message was supplied)</em></a>";
 		else
-		    echo "<li><a href=\"$github/$name/commits/$commit\">$msg</a></li>";
+		    echo -n "<a href=\"$github/$name/commits/$commit\">$msg</a>";
 		fi;
+		echo "</dd>";
 	    else
 		echo "Uh oh: something went wrong calling git on the repo '$repo', with name '$name', at '$path'";
 		exit 1;
@@ -74,6 +77,6 @@ for repo in $repos; do
 	exit 1;
     fi
 done
-echo "</ul>";
+echo "</dl>";
 
 exit 0;
